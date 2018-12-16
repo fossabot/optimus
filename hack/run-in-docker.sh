5c2916848e47e8cfdb32a6b3d0c993585514f81b
Victor Palade <victor@cloudflavor.io>
2018-12-16 15:52:59 +0100

    utils: update gen script.
    Signed-off-by: Victor Palade <victor@cloudflavor.io>

--- hack/run-in-docker.sh
+++ hack/run-in-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "utility needed: go get -u github.com/golang/dep/cmd/dep"
 
-docker run -ti -v $(pwd):/go/src/github.com/pi-victor/pipelines -w /go/src/github.com/pi-victor/pipelines golang:1.10-stretch /bin/bash
+docker run -ti -v $GOPATH:/go -w /go/src/github.com/cloudflavor/optimus golang:1.10-stretch /bin/bash
--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -7,7 +7,8 @@ set -o pipefail
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ${GOPATH}/src/k8s.io/code-generator)}
 
-vendor/k8s.io/code-generator/generate-groups.sh all \
-  github.com/pi-victor/pipelines/pkg/client  github.com/pi-victor/pipelines/pkg/apis \
-  cloudflavor.io:v1 \
+echo "You must have the k8s code-generator in $GOPATH/src/k8s.io/code-generator, othwerise this will fail"
+../../../k8s.io/code-generator/generate-groups.sh all \
+  github.com/cloudflavor/optimus/pkg/client  github.com/cloudflavor/optimus/pkg/apis \
+  pipelines:v1 \
   --go-header-file ${SCRIPT_ROOT}/assets/license-header.txt
