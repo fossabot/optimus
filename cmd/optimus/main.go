cef31ebb1389beff8ceb008061b767f5ea48e6fc
Victor Palade <victor@cloudflavor.io>
2018-12-22 21:20:34 +0100

    main: refactor app entrypoint.
    Signed-off-by: Victor Palade <victor@cloudflavor.io>

--- cmd/optimus/main.go
+++ cmd/optimus/main.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 Victor Palade.
+Copyright 2018 Cloudflavor Org contributors.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -10,44 +10,65 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package main
 
 import (
 	"flag"
-	"fmt"
+	//	"time"
+	"os"
 
 	"github.com/golang/glog"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	//	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	//	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 
 	optimusClient "github.com/cloudflavor/optimus/pkg/client/clientset/versioned"
+	//	optimusInformers "github.com/cloudflavor/optimus/pkg/client/informers/externalversions"
+	optimusController "github.com/cloudflavor/optimus/pkg/controller"
 )
 
 var (
-	kuberconfig = flag.String("kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
-	master      = flag.String("master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+	k8sConfig = flag.String("kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
+	master    = flag.String("master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
 )
+
+func init() {
+	// set logging capabilities. Flush logs and set stderr as default..
+	defer glog.Flush()
+	flag.Set("logtostderr", "true")
+	if os.Getenv("OPTIMUS_LOG_LEVEL") != "" {
+		flag.Set("v", os.Getenv("OPTIMUS_LOG_LEVEL"))
+	}
+	flag.Parse()
+}
 
 func main() {
 	flag.Parse()
-
-	cfg, err := clientcmd.BuildConfigFromFlags(*master, *kuberconfig)
+	cfg, err := clientcmd.BuildConfigFromFlags(*master, *k8sConfig)
 	if err != nil {
 		glog.Fatalf("Error building kubeconfig: %v", err)
 	}
-
-	pipelinesClient, err := optimusClient.NewForConfig(cfg)
+	localClient, err := optimusClient.NewForConfig(cfg)
 	if err != nil {
 		glog.Fatalf("Error building example clientset: %v", err)
 	}
-	list, err := pipelinesClient.OptimusV1().Pipelines("optimus").List(metav1.ListOptions{})
+
+	k8sClient, err := kubernetes.NewForConfig(cfg)
 	if err != nil {
-		glog.Fatalf("Error listing all databases: %v", err)
+		glog.Fatalf("Error creating new kubernetes client, %#v", err)
 	}
 
-	for _, pipeline := range list.Items {
-		fmt.Printf("THIS IS THE PIPELINE %#v", pipeline.GetName())
-		break
-	}
+	//	optimusInformersFactory := optimusInformers.NewSharedInformerFactory(localClient, time.Second*10)
+	//	k8sInformerFactory := informers.NewSharedInformerFactory(k8sClient, time.Second*10)
+
+	controller := optimusController.NewController(k8sClient, localClient)
+
+	stop := make(chan struct{})
+	defer close(stop)
+	go controller.Run(stop)
+
+	select {}
 }
